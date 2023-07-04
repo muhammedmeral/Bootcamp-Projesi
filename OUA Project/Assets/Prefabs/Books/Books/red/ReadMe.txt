b0ad2f4307e5df4003e52017b4af78af5be7dcbf
muhammedmeral <mmeral3382@gmail.com>
2023-07-04 22:25:52 +0300

    git merge denemesi 4

---
+++ OUA Project/Assets/Prefabs/Books/Books/red/ReadMe.txt
@@ -0,0 +1,9 @@
+The book 
+by Pavel Kutejnikov
+
+CC0 / Public domain
+
+If you want to support me, you can add to your wishlist / buy my pixelart game "Tzakol in Exile":
+https://store.steampowered.com/app/1764840/Tzakol_in_Exile/
+
+Good luck!
---
+++ OUA Project/Assets/Scripts/zaman_sayaci.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class zaman_sayaci : MonoBehaviour
+{
+    float saat = 05;
+    float dakika = 59;
+
+    string ikiNokta = ":";
+
+    public TextMeshProUGUI zamanSayaci;
+    
+
+
+
+    private void Start()
+    {
+        
+
+        if (zamanSayaci!=null)
+        {
+            zamanSayaci.text = (int)saat + ikiNokta + (int)dakika; 
+        }
+    }
+
+
+
+    private void Update()
+    {
+
+        
+        if (zamanSayaci != null)
+        {
+            dakika -= Time.deltaTime;
+
+            if (dakika < 10) {
+                zamanSayaci.text = (int)saat + ikiNokta+"0" + (int)dakika;
+            }
+            else
+                zamanSayaci.text = (int)saat + ikiNokta + (int)dakika;
+
+            if (dakika < 0)
+            {
+                saat--;
+                dakika = 59;
+            }
+            
+        }
+
+    }
+
+
+    
+}
+
