ac13f1f5124dfd081ed0071c012b08e0bcd2937d
muhammedmeral <mmeral3382@gmail.com>
2023-07-06 16:11:25 +0300

    Karakter prefabı ve boyutları düzenlendi
    Karakter boyutları ve pref yapısı düzenlendi. Merge için hazır hal getirildi.

--- OUA Project/Assets/Scripts/CanavarController.cs
+++ OUA Project/Assets/Scripts/CanavarController.cs
@@ -6,51 +6,64 @@ using UnityEngine.AI;
 public class CanavarController : MonoBehaviour
 {
     public Animator anim;  //1. Canavarýn animasyonlarýný kontrol edecek component.
-    public GameObject hedef; //2. Canavarýn hedefi olacak. (Oyuncu)
-    //[SerializeField] float hareketHizi; //3. Canavarýn hareket hýzýný tutacak deðiþken.
-    NavMeshAgent nmesh; //4. Canavardaki navmesh agent compenentini tutacak deðiþken.
-    bool isAttack = false;
     
-    public float mesafe; //5. Canavar ve karaktermiz arasýndaki mesafeyi ölçecek deðiþken. (Bu mesafeye göre ilgili animasyon ve olaylarýn çalýþmasý kontrol edilecek.)
+    public GameObject hedef; //2. Canavarýn hedefi olacak. (Oyuncu)
+   
+    NavMeshAgent nmesh; //3. Canavardaki navmesh agent compenentini tutacak deðiþken.
+    
+    public float mesafe; //4. Canavar ve karaktermiz arasýndaki mesafeyi ölçecek deðiþken. (Bu mesafeye göre ilgili animasyon ve olaylarýn çalýþmasý kontrol edilecek.)
 
 
 
     private void Start()
     {
-
-        nmesh = GetComponent<NavMeshAgent>(); //6. Navmesh componentimizi cache ettik.
-        
+        nmesh = GetComponent<NavMeshAgent>(); // Navmesh componentimizi cache ettik.
 
 
     }
 
     private void Update()
     {
-       
-        
-        mesafe = Vector3.Distance(hedef.transform.position,transform.position); //10. Bu kýsým, canavarla karakterin arasýndaki mesafeyi ölçecek.
+       mesafe = Vector3.Distance(hedef.transform.position,transform.position); //Bu kýsým, canavarla karakterin arasýndaki mesafeyi ölçecek.
 
-        nmesh.destination = hedef.transform.position;  //7. Bu kod satýrý sayesinde, canavar haritada karakterimizi takip edecek.
+       nmesh.destination = hedef.transform.position;  // Bu kod satýrý sayesinde, canavar haritada karakterimizi takip edecek.
 
-        
-        if (mesafe < 8 && mesafe>2.5f) //11. Bu kýsýmda canavar, karaktere doðru koþacak.
+        Debug.Log("Mesafe: " + mesafe);
+
+        if (mesafe > 8f) //walk anim çalýþacak.
         {
-            //Bu kýsýmda koþma ses efekti kullanýlacak.
-            walkToRun();
-            nmesh.speed = 2.4f;
-            
+            anim.SetBool("isAttack1", false);
+            anim.SetBool("isAttack2", false);
+            anim.SetBool("isWalk", true);
+            anim.SetBool("isRun", false);
+            //bu kýsýmda yürüme ses efekti çalacak.
+        }
+
+        else if (mesafe >= 2.5f && mesafe <= 8f) //iddle ve run animler çalýþacak.
+        {
+            anim.SetBool("isAttack1", false);
+            anim.SetBool("isAttack2", false);
+            anim.SetBool("isWalk", false);
+            anim.SetTrigger("walkToIddle");
+            //bu kýsýmda kükreme ses efekti çalacak.
+            StartCoroutine(IddleBekleme());
+            anim.SetBool("isRun", true);
+            //bu kýsýmda koþma ses efekti çalacak.
+        }
+         
+        else if (mesafe < 2.5f) //Attack animasyonlarý çalýþacak.
+        {
+            anim.SetBool("isWalk", false);
+            anim.SetBool("isRun", false);
+            anim.SetBool("isAttack1", true);
+            //burada attack1 animasyonu çalacak.
+            anim.SetBool("isAttack2", true);
+            //burada attack2 anias
 
         }
-        if (mesafe >= 9 ) //13. Bu kýsýmda canavar karaktere doðru yürüyecek.
-        {
-            runToWalk();
-            nmesh.speed = 1.8f;
 
-        }
-        
-        
 
-        
+
 
 
 
@@ -58,42 +71,6 @@ public class CanavarController : MonoBehaviour
 
 
     }
-
-    void walkToRun() //8. Bu fonksiyonda, canavar yürürken, koþma durumuna geçecek.
-    {
-        anim.SetBool("isWalk", false);
-        anim.SetTrigger("walkToIddle");
-        StartCoroutine(IddleBekleme());
-        anim.SetBool("isRun", true);
-        
-        
-        
-    }
-    void runToWalk() //9. Bu fonksiyonda canavar koþarken, yürüme durumuna geçecek.
-    {
-        anim.SetBool("isRun", false);
-        anim.SetBool("isWalk", true);
-        
-    }
-
-    void runToAttack()
-    {
-        
-        anim.SetBool("isRun", false);
-        anim.SetTrigger("isAttack1");
-        anim.SetBool("isAttack2", true);
-
-       
-
-    }
-    //void attackToRun()
-    //{
-        
-        
-    //}
-
-
-
 
     IEnumerator IddleBekleme() // 12 .walk animden iddle anime geçtiði sýrada canavarýn yerinde beklemesi için bu yapýyý kullandýk.
     {
