9e1d9be94e2c4cde260079b7f2049bd0b309fe79
Muhammed <mmeral3382@gmail.com>
2023-07-08 21:34:49 +0300

    Arayüz bileşenlerinin kodlama kısmı bitirildi
    Silah kamera ve mermi renkleri, görünümleri vs yapıldı.

--- OUA Project/Assets/Scripts/AtesSistemi.cs
+++ OUA Project/Assets/Scripts/AtesSistemi.cs
@@ -12,15 +12,13 @@ public class AtesSistemi : MonoBehaviour
     public ParticleSystem wallEffect; //21. Duvardan çýkacak mermi efekti.
     float sonTiklama; //8. mouseye týkladýðýmýz zamaný tutan deðiþken.
     float tiklamaBeklemeSuresi = 0.65f; //9. mouseye tekrar týklamamýz için geçmesi gereken süre.
-
+    float atesSiniri = 6f;//30. Ateþ etme miktarýný sýnýrlayan deðiþken.
     public GameObject bloodObje;  //13. Kan efektini çýkaracak prefabýmýz.
     Vector3 mermiKonum; //14. raycast ýþýnýnýn canavara çarptýðý pozisyonu tutacak deðiþken
     Quaternion hitRotation; //15. raycast ýþýnýnýn canavara çarptýðý açýyý tutacak deðiþken
-
     public GameObject wallObje; //22. Duvar tagýna sahip nesnelere vurduðumuzda çýkaracak prefabýmýz
     Vector3 wallKonum; //23. raycast ýþýnýnýn canavara çarptýðý pozisyonu tutacak deðiþken.
-    Quaternion wallRotation; //24. raycast açýsýnýn duvar tagýna sahip nesnelere çarptýðý açý
-
+    Quaternion wallRotation; //24. raycast açýsýnýn duvar tagýna sahip nesnelere çarptýðý açý    
 
 
     void Start()
@@ -31,29 +29,21 @@ public class AtesSistemi : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) //2. Bu kýsýmda mouseye týkladýðýmýzda ekranda çýkacak particle effectleri kontrol edeceðiz.
+        if (Input.GetMouseButtonDown(0)&&atesSiniri>0) //2. Bu kýsýmda mouseye týkladýðýmýzda ekranda çýkacak particle effectleri kontrol edeceðiz.
         {
             if (Time.time - sonTiklama > tiklamaBeklemeSuresi)
             {
                 AtesEt(); //4. Burada ateþ etme iþlemimiz gerçekleþiyor.
                 StartCoroutine(muzzleBekleme());//11. Burada muzzle effect çalýþýyor.
-
+                atesSiniri--;         
                 sonTiklama = Time.time; //10. Son týklama zamaný ile ilgili deðiþkeni güncelliyoruz.
-
-
-
             }
         }
-
-
-
-
 
     }
 
     void AtesEt() //3. Ateþ etme iþlemimiz ve ateþ ettiðimizde çýkacak particle sistemler bu fonksiyonda olacak.
     {
-
         Ray ray = kamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
 
@@ -63,15 +53,12 @@ public class AtesSistemi : MonoBehaviour
             {
                 if (hit.collider.name == "Enemy") //16. Eðer ýþýnýn çarptýðý nesne, canavarsa, bu koþulun içindeki kod dizisi çalýþacak.
                 {
-
-
                     hit.collider.gameObject.GetComponent<CanavarController>().HasarAl(); //17. Burada canavarýmýz hasar alacak.
                     mermiKonum = hit.point; //18. ýþýnýn konumunu ilgili deðiþkene atadýk.
                     hitRotation = Quaternion.LookRotation(hit.normal);//19. ýþýnýn rotasyonunu ilgili deðiþkene atadýk.
                     Instantiate(bloodObje, mermiKonum, hitRotation); //20. burada kan efekti instance ettik.
-
-
                 }
+
                 if (hit.collider.gameObject.CompareTag("duvar")) //25. Eðer ýþýnýn çarptýðu nesne duvar tagýna sahipse bu koþulun içindeki olaylar gerçekleþecek.
                 {
                     wallKonum = hit.point; //26. ýþýnýn konumunu ilgili deðiþkene atadýk.
@@ -80,8 +67,7 @@ public class AtesSistemi : MonoBehaviour
                     {
                         Destroy(wallObje); //28. Önce, daha önce bir obje varsa onu yok ettik ki nullreferance hatasý almayalým diye.
                     }
-                    wallObje =Instantiate(wallObje, wallKonum, wallRotation); //29. Ardýndan da yeni bir obje ürettik.
-                    
+                    wallObje =Instantiate(wallObje, wallKonum, wallRotation); //29. Ardýndan da yeni bir obje ürettik.                    
 
                 }
 
--- OUA Project/Assets/Scripts/KarakterAnimasyonController.cs
+++ OUA Project/Assets/Scripts/KarakterAnimasyonController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class KarakterAnimasyonController : MonoBehaviour
 {
     //crosshair birden fazla fonksiyonun içinde kullanýlacak(silah tutma,ateþ etme, kamera tutma,fotoðraf çekme vs.) unutma!!!!
@@ -11,36 +11,107 @@ public class KarakterAnimasyonController : MonoBehaviour
     public GameObject pistol; //3.karakterin elindeki pistolü animasyonlar sýrasýnda açýp kapatmak için kullandýðýmýz deðiþken.
     bool elindeSilahVar = true; //4.karakterin elindeki nesneden diðer nesnelere geçiþte animasyon çakýþmasý olmamasý için kullandýðýmýz deðiþken.
     bool elindeKameraVar = false; //5.karakterin elindeki nesneden diðer nesnelere geçiþte animasyon çakýþmasý olmamasý için kullandýðýmýz deðiþken.
+    float atesLimiti = 6f; //16. Ateþ etme animasyonunu limitleyen deðiþken
+    float fotografLimiti = 5f; //16.1 Fotoðraf çekme animasyonunu limitleyen deðiþken.
+    float sonTiklama; //17. Son ateþ etme süresini tutacak deðiþken.
+    float tiklamaBeklemeSuresi = 0.65f;//18. Ateþ etme ve fotoðraf çekme animasyonlarýnýn bekleme süresini sýnýrlayan deðiþken.
+    public GameObject mermiBir;
+    public GameObject mermiIki;
+    public GameObject mermiUc;
+    public GameObject mermiDort;
+    public GameObject mermiBes;
+    public GameObject mermiAlti;
+    public GameObject silahSembol;
+    public GameObject kameraSembol;
+    Color deaktifColor = new Color(1f,1f,1f,0.05f);
+    Color aktifColor = new Color(25.5f, 25.5f, 25.5f, 25.5f);
+    RawImage silahRaw;
+    RawImage kameraRaw;
+    RawImage mermiBirRaw;
+    RawImage mermiIkiRaw;
+    RawImage mermiUcRaw;
+    RawImage mermiDortRaw;
+    RawImage mermiBesRaw;
+    RawImage mermiAltiRaw;
 
-   
 
-    
+
     void Start()
     {
         elKamerasi.SetActive(false); //12.baþlangýçta olmasýný istediðimiz durumlarý saðlamak için(elinde silah olmasý, silahýn görünür olmasý vs.) atadýðýmýz deðerler
         elindeSilahVar = true; //12.1 baþlangýçta olmasýný istediðimiz durumlarý saðlamak için(elinde silah olmasý, silahýn görünür olmasý vs.) atadýðýmýz deðerler
         elindeKameraVar = false; //12.2 baþlangýçta olmasýný istediðimiz durumlarý saðlamak için(elinde silah olmasý, silahýn görünür olmasý vs.) atadýðýmýz deðerler
+        silahRaw = silahSembol.GetComponent<RawImage>();
+        kameraRaw = kameraSembol.GetComponent<RawImage>();
+        mermiBirRaw=mermiBir.GetComponent<RawImage>();
+        mermiIkiRaw=mermiIki.GetComponent<RawImage>();
+        mermiUcRaw=mermiUc.GetComponent<RawImage>();
+        mermiDortRaw=mermiDort.GetComponent<RawImage>();
+        mermiBesRaw=mermiBes.GetComponent<RawImage>();
+        mermiAltiRaw=mermiAlti.GetComponent<RawImage>();
+        silahRaw.color = aktifColor;
+        kameraRaw.color = deaktifColor;
+        mermiBirRaw.color = aktifColor;
+        mermiIkiRaw.color = aktifColor;
+        mermiUcRaw.color = aktifColor;
+        mermiDortRaw.color = aktifColor;
+        mermiBesRaw.color = aktifColor;
+        mermiAltiRaw.color = aktifColor;
+
     }
 
     void Update()
     {
        
-        if (Input.GetMouseButtonDown(0) && elindeSilahVar == true) //14.atanan tuþa basýldýðýnda ve karakterin elinde silah varsa ateþ etme fonksiyonu çalýþýr.
+        if (Input.GetMouseButtonDown(0) && elindeSilahVar == true&&atesLimiti>0) //14.atanan tuþa basýldýðýnda ve karakterin elinde silah varsa ateþ etme fonksiyonu çalýþýr.
         {
-            AtesEtme();
-            //ateþ edildiðinde hasar verme sistemi daha sonra burada kullanýlacak.
+            if (Time.time-sonTiklama>tiklamaBeklemeSuresi) //19. Bekleme süresi ve ateþ etme limitini burada kullandýk.
+            {
+                AtesEtme();
+                              
+                //ateþ edildiðinde hasar verme sistemi daha sonra burada kullanýlacak.
+                sonTiklama = Time.time;
+
+                switch (atesLimiti) //Ateþ ettikçe mermi sayýsýný gösteren sembollerin ekrandan silinmesini saðlayacak yapý.
+                {
+                    case 6: mermiBir.SetActive(false);                        
+                        break;
+                    case 5:mermiIki.SetActive(false);
+                        break;
+                    case 4:mermiUc.SetActive(false);
+                        break;
+                    case 3:mermiDort.SetActive(false);
+                        break;
+                    case 2: mermiBes.SetActive(false);
+                        break;
+                    case 1: mermiAlti.SetActive(false);
+                        break;
+                    default:
+                        break;
+                }
+                atesLimiti--;
+            }
         }
+
+        //ateþ etme limiti dolduðunda silahýn boþ olduðunu anlayacaðýmýz ses efekti burada bir if açýlarak yapýlacak.
 
         else //15.eðer 14.maddedeki þartlar saðlanmýyorsa ateþ etme animasyonunun durmasý saðlanýr.
         {
             anim.SetBool("atesEdiyorMu", false);
         }
 
-        if (Input.GetMouseButtonDown(0) && elindeKameraVar == true) //14.1 atanan tuþa basýldýðýnda ve karakterin elinde kamera varsa fotoðraf çekme fonksiyonu çalýþýr.
+        if (Input.GetMouseButtonDown(0) && elindeKameraVar == true&&fotografLimiti>0) //14.1 atanan tuþa basýldýðýnda ve karakterin elinde kamera varsa fotoðraf çekme fonksiyonu çalýþýr.
         {
-            FotoCekme();
-            //fotoðraf çekildiðinde fotoðraflarýn depolamada kaydedilmesi ve fotoðraf çekilme sýrasýnda raycast yöntemiyle sorgulamanýn yapýlmasý iþlemleri buraya gelicek.
+            if (Time.time - sonTiklama > tiklamaBeklemeSuresi)
+            {
+                FotoCekme();
+                fotografLimiti--;
+                sonTiklama = Time.time;                
+            }
+            
+            //--fotoðraf çekildiðinde fotoðraflarýn depolamada kaydedilmesi ve fotoðraf çekilme sýrasýnda raycast yöntemiyle sorgulamanýn yapýlmasý iþlemleri buraya gelicek.
         }
+        //Fotoðraf çekme limiti dolduðunda kullanýcýnýn anlamasýný saðlayacak ses efekti burada çalacak.
 
         else //15.1 eðer 14.1.maddedeki þartlar saðlanmýyorsa fotoðraf çekme animasyonunun durmasý saðlanýr.
         {
@@ -53,19 +124,31 @@ public class KarakterAnimasyonController : MonoBehaviour
             elindeSilahVar = false;
             SilahiBirakipKamerayýTutma();
 
-
+            kameraRaw.color = aktifColor;
+            silahRaw.color = deaktifColor;
+            mermiBirRaw.color = deaktifColor;
+            mermiIkiRaw.color = deaktifColor;
+            mermiUcRaw.color = deaktifColor;
+            mermiDortRaw.color = deaktifColor;
+            mermiBesRaw.color = deaktifColor;
+            mermiAltiRaw.color = deaktifColor;
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2) && elindeKameraVar == true) //13.1 atadýðýmýz tuþa basýldýðýnda karakter kamerayý býrakýp silahý alýr. 4 ve 5. maddedeki deðiþkenlere olmasý gereken deðerler atandý. 
         {
-
             elindeSilahVar = true;
             elindeKameraVar = false;
             KamerayiBirakipSilahTutma();
 
-        }
-
-
+            kameraRaw.color = deaktifColor;
+            silahRaw.color = aktifColor;
+            mermiBirRaw.color = aktifColor;
+            mermiIkiRaw.color = aktifColor;
+            mermiUcRaw.color = aktifColor;
+            mermiDortRaw.color = aktifColor;
+            mermiBesRaw.color = aktifColor;
+            mermiAltiRaw.color = aktifColor;
+        }        
 
     }
 
@@ -88,18 +171,12 @@ public class KarakterAnimasyonController : MonoBehaviour
         StartCoroutine(SilahAlmaBekletme());
         anim.SetTrigger("kameraBirakirkenSilahAlma");
         anim.SetTrigger("silahAlirkenTutma");
-
     }
 
     void AtesEtme() //10.karakterin ateþ etme animasyonunu gerçekleþtiren fonksiyon.
     {
-        anim.SetBool("atesEdiyorMu", true);
-
-        
+        anim.SetBool("atesEdiyorMu", true);        
     }
-
-
-
 
     IEnumerator SilahAlmaBekletme() //11.  7.maddedeki iþlemi gerçekleþtiren coroutine
     {
@@ -107,6 +184,7 @@ public class KarakterAnimasyonController : MonoBehaviour
         pistol.SetActive(true);
         elKamerasi.SetActive(false);
     }
+
     IEnumerator KameraAlmaBekletme()
     {
         yield return new WaitForSecondsRealtime(1f);
